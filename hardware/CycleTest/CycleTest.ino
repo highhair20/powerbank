024a3c32c7738ca625263f4b4355a2b98b94cafa
highhair20 <jkelly09@gmail.com>
2015-11-04 11:41:25 -0800

    add server files

--- hardware/CycleTest/CycleTest.ino
+++ hardware/CycleTest/CycleTest.ino
@@ -1,8 +1,5 @@
-#include <Event.h>
-#include <Timer.h>
-
 /*
-  PowerStrip
+  CycleTest
 
   Circuit:
   * Arduino Uno
@@ -21,33 +18,33 @@
                          -| IOREF         (pwm) 11 |- Wifi comm via SPI
                          -| 3.3V          (pwm) 10 |- WiFi Slave Select (SS)
     Power to Wifi Shield -| 5V            (pwm)  9 |- Plug 6
-                         -| GND                  8 |- Plug 5
-                         -| GND                    |
+            Relay Ground -| GND                  8 |- Plug 5
+              PIR Ground -| GND                    |
                          -| Vin                  7 |- Handshake
                           |               (pwm)  6 |- Plug 4
-                         -| A0            (pwm)  5 |- Plug 3
-                         -| A1                   4 |- WiFi SS for SD card
+               PIR Power -| A0            (pwm)  5 |- Plug 3
+         PIR Digital Out -| A1                   4 |- WiFi SS for SD card
                          -| A2            (pwm)  3 |- Plug 2
                          -| A3                   2 |- Plug 1
                   Plug 8 -| A4 (SDA)        TX-> 1 |-
                   Plug 7 -| A5 (SCL)        RX<- 0 |-
                           --------------------------
 
-  created 2015.09.28
-  by Jason Kelly
-
-  This code is in the public domain.
-
   Arduino communicates with the WiFi shield using the
   SPI (Serial Peripheral Interface) bus on digital pins 11, 12, and 13.
+
+  Created 2015.09.28
+  by Jason Kelly
 */
 
+#include <Event.h>
 #include <Timer.h>
 
-Timer t;
 
+Timer t;
 int flashEvents[8];
-int pins[8] = {
+
+int plugPins[8] = {
   2,  // Plug 1
   3,  // Plug 2
   5,  // Plug 3
@@ -58,41 +55,48 @@ int pins[8] = {
   A4  // Plug 8
 };
 int level[] = {HIGH, LOW};
-int numPins = sizeof( pins ) / sizeof( int );
+int numPins = sizeof( plugPins ) / sizeof( int );
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Cycle - begin setup");
 
   // set the pin modes
-  Serial.print("sizeof pins: ");
   Serial.println(numPins);
-  for (int i  = 0; i < sizeof(pins); i++) {
-    pinMode(pins[i], OUTPUT);
+  for (int i  = 0; i < numPins; i++) {
+    pinMode(plugPins[i], OUTPUT);
   }
 
-  flashAll(3);
+  // set high
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(A5, HIGH);
+  digitalWrite(A4, HIGH);
+
+  // test flash
+  flashAll();
+
+  //
+  t.every(1000 * 30, flicker);
 
 }
+
 
 void loop() {
-  for (int i  = 0; i < numPins; i++) {
-    digitalWrite(pins[i], level[0]);
-    delay(100);
-    digitalWrite(pins[i], level[1]);
+  t.update();
+}
+
+
+void flashAll() {
+  for (int i = 0; i < numPins; i++) {
+    flashEvents[i] = t.oscillate(plugPins[i], 250, LOW, 3);
   }
 }
 
 
-void flashAll(int times) {
-  for (int i = 0; i < times; i++) {
-    for (int j = 0; j < numPins; j++) {
-      digitalWrite(pins[j], level[0]);
-    }
-    delay(500);
-    for (int j = 0; j < numPins; j++) {
-      digitalWrite(pins[j], level[1]);
-    }
-    delay(500); 
-  }
+void flicker() {
+  t.oscillate(2, 30, LOW, 10);
+  t.oscillate(3, 30, LOW, 10);
 }
